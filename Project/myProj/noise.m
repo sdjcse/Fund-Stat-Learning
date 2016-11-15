90f5bb68bec5e59ebf0c6594a71131172e148726
sdj <sdjcse1@gmail.com>
2016-11-15 15:24:20 -0700

    corrected code

--- Project/myProj/noise.m
+++ Project/myProj/noise.m
@@ -1,12 +1,25 @@
 %function [newX] = noisAdded(X,y,eta)
-
-X = trainImages;
-y = trainLabels;
+load('data.mat');
+X = testImages;
+y = testLabels;
 load('theta.mat');
+for i = 1:length(y)
+  if y(i) == 0
+    y(i) = -1;
+  end
+end
 exMat = sign(xderivative(y,theta,X));
-newX = X + (0.0025*exMat);
+newX = X + (0.025*exMat);
+
+
 
 X = newX;
+ figure;
+ h = [];
+ for iter = 1:100
+     h(iter) = subplot(10,10,iter);
+     imshow(imresize(reshape(X(iter+1084,:),[28,28]),4));
+ end
 load('theta.mat');
 b = theta(size(theta,1),1);
 theta(size(theta,1),:) = [];
@@ -18,7 +31,8 @@ for j=1:size(y,1)
   if prob(j) > 0.5
     output(j) = 1;
   else
-    output(j) = 0;
+    %output(j) = 0;
+    output(j) = -1;
   end
 
   if output(j) == y(j)
